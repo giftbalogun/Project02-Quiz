fce45eaca95e8b1e76560bda929f267338923289
Gift Balogun <blgnbalogun52@gmail.com>
2018-12-03 02:13:46 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'messages.php';
+
+//site specific configuration declartion
+define( 'BASE_PATH', 'http://localhost/quiz/');
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USERNAME', 'root');
+define( 'DB_PASSWORD', '');
+define( 'DB_NAME', 'user-login');
+
+function __autoload($class)
+{
+	$parts = explode('_', $class);
+	$path = implode(DIRECTORY_SEPARATOR,$parts);
+	require_once $path . '.php';
+}
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php 
+ob_start();
+session_start();
+require_once 'config.php'; 
+?>
+<?php 
+	if( !empty( $_POST )){
+		try {
+			$user_obj = new Cl_User();
+			$data = $user_obj->login( $_POST );
+			if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+				$_SESSION['success'] = 'You are logged in successfully';
+				header('Location: home.php');exit;
+			}
+		} catch (Exception $e) {
+			$error = $e->getMessage();
+			$_SESSION['error'] = $error;
+		}
+	}
+	//print_r($_SESSION);
+	if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']){
+		header('Location: home.php');exit;
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RevisePAQ Online</title>
+	<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/login.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+	<div class="container">
+		<div class="login-form">
+			<?php require_once 'templates/message.php';?>
+			<h1 class="text-center">RevisePAQ Online Login Here</h1>
+			<div class="form-header">
+				<i class="fa fa-user"></i>
+			</div>
+			<form id="login-form" method="post" class="form-signin" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+				<input name="email" id="email" type="email" class="form-control" placeholder="Email address" autofocus> 
+				<input name="password" id="password" type="password" class="form-control" placeholder="Password"> 
+				<button class="btn btn-block bt-login" type="submit">Sign in</button>
+			</form>
+			<div class="form-footer">
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<i class="fa fa-lock"></i>
+						<a href="forget_password.php"> Forgot password? </a>
+					
+					</div>
+					
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<i class="fa fa-check"></i>
+						<a href="register.php"> Sign Up </a>
+					</div>
+				</div>
+			</div>
+			<br/>
+			<div class="text-center well">
+				<p>RevisePAQ Online Question Platform</p>
+			</div>
+		</div>
+	</div>
+	<!-- /container -->
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/login.js"></script>
+  </body>
+</html>
+<?php ob_end_flush(); ?>
+<?php unset($_SESSION['success'] ); unset($_SESSION['error']);  ?>    
